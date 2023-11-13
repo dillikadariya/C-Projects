10aadfea515192dfed03ef781e02e0cb9569eba9
Dilliram Kadariya <150565897+dillikadariya@users.noreply.github.com>
2023-11-13 13:45:15 -0600

    TicTacToe CS I.cpp
    This is a C++ program for a two player Tic-tac-toe game.

--- TicTacToe CS I.cpp
+++ TicTacToe CS I.cpp
@@ -1,5 +1,5 @@
-// TicTacToe CS I.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // Dilli Kadariya
+// TicTacToe Game using 2D Arrays
 
 #include <iostream>
 using namespace std;
@@ -7,81 +7,91 @@ using namespace std;
 void takeTurn(int player, char board[][3]);
 void displayBoard(char board[][3]);
 void pickSpace(int player, char board[][3]);
-
+bool isWinner(char board[][3]);
+bool isValid(int row, int col, char board[][3]);
 
 char board[3][3] = { { ' ', ' ', ' ' },
-	                 { ' ', ' ', ' ' }, 
-	                 { ' ', ' ', ' ' } };
-
+                     { ' ', ' ', ' ' },
+                     { ' ', ' ', ' ' } };
 
 int main()
-{    
-	
-	int count = 0;
-	taketurn((count % 2 + 1), board);
-
-}
-
-void takeTurn(int player, char board[][3]) 
 {
-	displayBoard();
-	pickSpace();
+    int count = 0;
+    int player = 1; 
+    while (!isWinner(board))
+    {
+        displayBoard(board);
+        pickSpace(player, board);
+        player = (player % 2) + 1;
+    }
+        displayBoard(board);
+        cout << "Player " << (player % 2) + 1 << " wins!" << endl;
+    return 0;
 }
 
 void displayBoard(char board[][3])
 {
-	const int row = 3, col = 3;
-	
-	int board[row][col] = { { ' ', ' ', ' ' },
-					 { ' ', ' ', ' ' },
-					 { ' ', ' ', ' ' } };
-	for (int row = 0; row < 3; row++)
-		cout << "____";
-	cout << "|____";
-	{
-		for (int col = 0; col < 3; col++)
-		{
-			cout << board[row][col] << "__|__ ";
-		}
-		cout << endl;
-	}
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 3; col++)
+        {
+            cout << board[row][col];
+            if (col < 2) 
+            {
+                cout << " | ";
+            }
+        }
+        cout << endl;
+        if (row < 2) {
+            cout << "---------" << endl;
+        }
+    }
 }
 
 void pickSpace(int player, char board[][3])
 {
-	int row;
-	int col;
-	do
-	{
-		cout << "Enter your row selection : ";
-		cin >> row;
-		cout << "Enter your column selection : ";
-		cin >> col;
-	} while (isValid(row,col,board));
-	board[row][col] = '0'; 'x';
+    int row, col;
+    do {
+        cout << "Player " << player << ", enter your row selection (1-3): ";
+        cin >> row;
+        cout << "Player " << player << ", enter your column selection (1-3): ";
+        cin >> col;
+    } while (!isValid(row - 1, col - 1, board)); 
+
+    board[row - 1][col - 1] = (player == 1) ? 'X' : 'O';
 }
-	bool isValid(int row, int col, char board[][3])
-	{
-		if (row < 1 || row>3)
-			return false;
-		if (col < 1 || col>3)
-			return false;
-		if (board[row][col] != '____')
-			return false;
-		else
-		return true;
-	}
-	
-	char isWinner(char board[][3])
-	{
-		if (board[0][0] = board[0][1] = board[0][2])
-			cout << "You Won" << endl;
-		if (board[0][0] = board[0][1] = board[0][2])
-			cout << "You Won" << endl;
-		if (board[0][0] = board[0][1] = board[0][2])
-			cout << "You Won" << endl;
 
-	}
+bool isValid(int row, int col, char board[][3])
+{
+    if (row < 0 || row >= 3 || col < 0 || col >= 3) {
+        cout << "Invalid input. Row and column should be between 1 and 3." << endl;
+        return false;
+    }
+    if (board[row][col] != ' ') {
+        cout << "That space is already taken. Choose a different one." << endl;
+        return false;
+    }
+    return true;
+}
 
-
-
+bool isWinner(char board[][3]) {
+    // Check for a winner in each row
+    for (int row = 0; row < 3; row++) {
+        if (board[row][0] == board[row][1] && board[row][1] == board[row][2] && board[row][0] != ' ') {
+            return true;
+        }
+    }
+    // Check for a winner in each column
+    for (int col = 0; col < 3; col++) {
+        if (board[0][col] == board[1][col] && board[1][col] == board[2][col] && board[0][col] != ' ') {
+            return true;
+        }
+    }
+    // Check for a winner in diagonals
+    if ((board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') ||
+        (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')) {
+        return true;
+    }
+    // If no winner, return false
+    return false;
+}
